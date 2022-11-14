d66a73539c3c26a8709551f8ed418975898c4bfb
1zo10lan <115670107+1zo10lan@users.noreply.github.com>
2022-11-14 19:05:10 +0300

    Upload video.html instead

---
+++ videojs.js
@@ -0,0 +1,21 @@
+var video = document.getElementById("myVideo");
+var btn = document.getElementById("myBtn");
+var navbar = document.querySelector("header")
+
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "II";
+  } else {
+    video.pause();
+    btn.innerHTML = "&triangleright;";
+  }
+}
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 695) {
+    navbar.style.opacity = "0";
+  } else {
+    navbar.style.opacity = "1";
+  }
+});
